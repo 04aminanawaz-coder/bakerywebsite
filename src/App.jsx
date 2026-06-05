c6b09101689eff9e48c6e981521c9d705cfd974c
bakerywebsite <04.amina.nawaz@gmail.com>
2026-06-05 21:58:54 +0500

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
+import './App.css'
 import navbar from './componenets/navbar'
 import button from './componenets/button'
-import './App.css'
+
 
 function App() {
  
@@ -10,7 +11,7 @@ function App() {
     <>
     <navbar/>
    <button/>
- 
+
     </>
   )
 }
