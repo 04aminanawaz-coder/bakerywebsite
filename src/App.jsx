9333f6d140f5b3d08d846f68398db2120b1ac247
bakerywebsite <04.amina.nawaz@gmail.com>
2026-06-05 21:33:01 +0500

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import button from './componenets/button'
+import navbar from './componenets/navbar'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+  <div>
+    <navbar/>
+   <button/>
+  </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/componenets/navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const navbar = () => {
+  return (
+    <>
+<div className="w-full h-18 border-2 border-amber-800">
+    
+</div>
+    </>
+  );
+};
+
+export default navbar;
